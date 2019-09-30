5281810aaecbf4f7bfa6d376dac7b9ac8cb2814e
Sergio Chacón <schacon7@dxc.com>
2019-09-30 11:46:35 -0500

    cat

--- OTPs/el_salvador.js
+++ OTPs/el_salvador.js
@@ -2,8 +2,8 @@ module.exports = {
     token: 'eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vbWJhYXMuZGVzYS5jby5kYXZpdmllbmRhLmNvbS9hdXRoL3YxL2tleXN0b3JlLy53ZWxsLWtub3duL2p3a3MuanNvbiIsImtpZCI6IjE1Njc3MzYzNzAifQ.eyJhdWQiOiJEQVY6Q0xPVUQ6QVVUSCIsImV4cCI6MTU2ODY0OTM5NCwiaWF0IjoxNTY4NjQ4Nzk0LCJpc3MiOiJEQVY6Q0xPVUQ6QVVUSCIsInN1YiI6IjI2NmE1NzEwLWQ4OTktMTFlOS05NjFjLTQ3Nzc2MTFlOTA5NSIsInVzZSI6ImEiLCJwcm9kdWN0IjoiTU5VSU5HX1NWXzQ4IiwianRpIjoiMjkxZWExYTAtZDg5OS0xMWU5LTkzYzktMWQ1OWYzY2Q2OTczIn0.ZkeBZi1bELE7hDRIndfsRlPIPa14Hm_zxxiZ1MkT7EOqspbQucDaUIA594YAmV4f-zHDWqRRzxwrx7CB-2KaxNJaebwLrDlj3cIksnLZGaZ9n9wiQdmHmoZbuFTNhQimBQvKRZCMuemkPYMHrXkIyu967P23gQ-1U5iD-mSX-Teqg37f71nOYDX7H_FNqc8W9SRWfuoT0ZakyoF20DdjaIt8kkI3mCmrC7k5VEFYHDgj-6wDgwAE3vPjvwD2Y2QHPBnGiLnk6Nnu91LnGPweTX908uQG81tgi97uxFzl3jY1ciGf1E4pTEqmhygfZm5jMbxcbF12F0xY9rC-fhiT1w',
     workflow: {
         APPBOOT: 'ING001',
-        ING001: 'ING002',
-        ING002: 'TPC001',
+        ING001: 'PCL001',
+        PCL001: 'TPC001',
         TPC001: 'BEN001',
         BEN001: 'VIN001',
         VIN001: 'VIN002',
@@ -23,9 +23,14 @@ module.exports = {
             pais: 'SV'
         }
     },
-    ING002: {
+    PCL001: {
         status: 1,
-        payload: {}
+        payload: {
+            documento: {
+                tipo: "DUI",
+                numero: "11111111"
+            }
+    }
     },
     BEN001: {
         status: 1,
