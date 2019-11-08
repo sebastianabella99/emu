982dcad6392b13d57bbad04eeb46eb633f81c660
Chacon <schacon7@dxc.com>
2019-11-08 10:47:59 -0600

    FEAT

--- routes/data.js
+++ routes/data.js
@@ -1,34 +1,36 @@
 var express = require('express');
+const request = require('request');
 var router = express.Router();
 const path = require('path');
 
 // El query "type" solo es para desarrollo local.
 
 const respuesta = (req, res) => {
-  const query = req.query;
-  file = req.params.data_id;
-  // if( query.aliado ) {
-  //   file = `${file}-A${query.aliado}`
-  // }
-  if( query.pais ) {
-    file = `${file}-P${query.pais}`
-  }
-  // if( query.modulo ) {
-  //   file = `${file}-M${query.modulo}`
-  // }
-  // if( query.canal ) {
-  //   file = `${file}-C${query.canal}`
-  // }
-  // if( query.lenguaje ) {
-  //   file = `${file}-L${query.lenguaje}`
-  // }
-  file = `${file}.json`
-  console.log('Archivo enviado:', file);
-  if ( query.pais ) {
-    res.sendFile(file, { root: path.join(__dirname, `../dataService/${query.pais}`) });
-    return;
-  }
-  res.sendFile(file, { root: path.join(__dirname, '../dataService') });
+
+  var fullUrl = 'https://mbaas.desa.cam.davivienda.com/catalogo/v1'+ req.originalUrl;
+  
+  
+
+    request(fullUrl, { json: true }, (err, res2, body) => {
+      if (err || res2.body.length === 0) {
+        console.warn('====XXXXX>        ', fullUrl);
+        const query = req.query;
+        file = req.params.data_id;
+        if( query.pais ) {
+          file = `${file}-P${query.pais}`
+        }
+        file = `${file}.json`
+        if ( query.pais ) {
+          res.sendFile(file, { root: path.join(__dirname, `../dataService/${query.pais}`) });
+          return;
+        }
+        res.sendFile(file, { root: path.join(__dirname, '../dataService') });
+        return;
+       }
+       return res.json(res2.body);
+    });
+
+  
 };
 
 router.get('/:data_id', respuesta);
